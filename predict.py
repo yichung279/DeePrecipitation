d1ccb458d077b863df3a5b1271089de9209116d6
uno <st9007a@gmail.com>
2018-05-26 10:20:04 +0800

    Show precision, recall, f1 score of each label

--- predict.py
+++ predict.py
@@ -1,17 +1,19 @@
+#!/usr/bin/env python3
 import numpy as np
-from glob import glob
 from keras.models import load_model
-import os
-
-import trans
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+from sklearn.metrics import classification_report
 
 if __name__ == '__main__':
-    model = load_model('model/latest_model.h5')
-    data = np.load('data/dataEven_1.npy')[0]
-    images = model.predict(trans.preProcess(data))
-    print(images)
-    for i, image in enumerate(images):
-        trans.postProcess(image, 'image%d.png' %(i+1))
-        print('img saved')
+
+    model = load_model('model/deconv.keras.h5')
+
+    data = np.load('feature/valid.0.npy')
+    x_te, y_te = np.split(data, [9], axis = 3)
+
+    y_true = np.reshape(y_te, [-1])
+
+    y_pred = model.predict(x_te)
+    y_pred = np.argmax(y_pred, axis = 3)
+    y_pred = np.reshape(y_pred, [-1])
+
+    print(classification_report(y_true, y_pred, labels = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]))
