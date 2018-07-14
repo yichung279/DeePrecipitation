ee0b06bbd5db6a31c1d1f11c81ee8a4a53c3aa94
yichung <ql3895619@gmail.com>
2018-07-14 23:49:45 +0800

    mofdify predict.py for season

--- keras/deconv.py
+++ keras/deconv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import argparse
 from keras.models import Sequential
 from keras.layers import Dense, Conv2DTranspose, Conv2D, Flatten, MaxPooling2D, UpSampling2D, BatchNormalization, Activation, Dropout, regularizers
 from keras.optimizers import Adam
@@ -68,37 +69,30 @@ def build_model():
     model.add(Activation('relu'))
 
     model.add(Conv2DTranspose(filters = 256, kernel_size = (3, 3), padding = 'same'))
-    model.add(Dropout(0.2))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(Conv2DTranspose(filters = 256, kernel_size = (3, 3), padding = 'same'))
-    model.add(Dropout(0.2))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(UpSampling2D())
 
     model.add(Conv2DTranspose(filters = 256, kernel_size = (3, 3), padding = 'same'))
-    model.add(Dropout(0.2))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(Conv2DTranspose(filters = 256, kernel_size = (3, 3), padding = 'same'))
-    model.add(Dropout(0.2))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(UpSampling2D())
 
     model.add(Conv2DTranspose(filters = 128, kernel_size = (3, 3), padding = 'same'))
-    model.add(Dropout(0.2))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(Conv2DTranspose(filters = 128, kernel_size = (3, 3), padding = 'same'))
-    model.add(Dropout(0.2))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(UpSampling2D())
 
     model.add(Conv2DTranspose(filters = 64, kernel_size = (3, 3), padding = 'same'))
-    model.add(Dropout(0.2))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(Conv2DTranspose(filters = 3, kernel_size = (3, 3), padding = 'same', activation = 'softmax'))
@@ -108,19 +102,40 @@ def build_model():
     return model
 
 if __name__ == '__main__':
-    run_name = 'dropout(0.2)-all'
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('season', help='spring/summer/fall/winter')
+    args = parser.parse_args()
+    
+    if args.season == 'spring':
+        season = 'spring_'
+        train_size = 5388
+        valid_size = 5703 
+    elif args.season == 'summer':
+        season = 'summer_'
+        train_size = 1964
+        valid_size = 2202
+    elif args.season == 'fall':
+        season = 'fall_'
+        train_size = 764
+        valid_size = 725
+    elif args.season == 'winter':
+        season = 'winter_'
+        train_size = 5411
+        valid_size = 5827
+    else:
+        os._exit(0)
+    model_name = 'dropout0.2'
 
     batch_size = 72
-    validation_size = 72
     epochs = 100
 
-    train_loader = DataLoader(file_glob_pattern = 'feature/train.*.npy', batch_size = batch_size)
-    valid_loader = DataLoader(file_glob_pattern = 'feature/valid.*.npy', batch_size = validation_size)
-    model_ckpt = ModelCheckpoint('model/deconv.keras.h5', verbose = 1, save_best_only = True)
-    tensorboard = TensorBoard(log_dir='./logs/' + run_name, histogram_freq=0, write_graph=True, write_images=False)
+    train_loader = DataLoader(file_glob_pattern = 'feature/' + season + 'train.*.npy', batch_size = batch_size)
+    valid_loader = DataLoader(file_glob_pattern = 'feature/' + season + 'valid.*.npy', batch_size = batch_size)
+    model_ckpt = ModelCheckpoint('model/'+ model_name + '.' + season + 'model.keras.h5', verbose = 1, save_best_only = True)
+    tensorboard = TensorBoard(log_dir='./logs/' + season + model_name, histogram_freq=0, write_graph=True, write_images=False)
 
     model = build_model()
-    #model = load_model('model/deconv.keras.h5')
     model.compile(loss = 'categorical_crossentropy', optimizer = Adam(lr = 1e-4), metrics = ['accuracy'])
-    history = model.fit_generator(train_loader, steps_per_epoch = 10741 // batch_size, epochs = epochs, validation_data = valid_loader, validation_steps = 11482 // validation_size
+    history = model.fit_generator(train_loader, steps_per_epoch = train_size // batch_size, epochs = epochs, validation_data = valid_loader, validation_steps = valid_size // batch_size
             , callbacks = [model_ckpt, tensorboard])
