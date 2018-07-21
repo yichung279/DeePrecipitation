ab299776295af1ef1b5ba1ab1ee8654fe1bf1a2d
yichung <ql3895619@gmail.com>
2018-07-21 13:26:16 +0800

    add class weight in deconv.py

--- keras/deconv.py
+++ keras/deconv.py
@@ -3,7 +3,7 @@
 import numpy as np
 import argparse
 from keras.models import Sequential
-from keras.layers import Dense, Conv2DTranspose, Conv2D, Flatten, MaxPooling2D, UpSampling2D, BatchNormalization, Activation, Dropout, regularizers
+from keras.layers import Dense, Conv2DTranspose, Conv2D, MaxPooling2D, UpSampling2D, BatchNormalization, Activation, Dropout, Reshape 
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint, TensorBoard
 
@@ -17,8 +17,6 @@ set_session(tf.Session(config = config))
 from utils import DataLoader
 
 def build_model():
-    # TODO: try , kernel_regularizer=regularizers.l1_l2(0.01)
-    # model.add(Dropout(0.2))
     model = Sequential()
 
     model.add(Conv2D(filters = 64, kernel_size = (3, 3), padding = 'same', input_shape = (72, 72, 9)))
@@ -97,6 +95,8 @@ def build_model():
     model.add(Activation('relu'))
     model.add(Conv2DTranspose(filters = 3, kernel_size = (3, 3), padding = 'same', activation = 'softmax'))
 
+    model.add(Reshape((-1, 3)))
+
     model.summary()
 
     return model
@@ -129,17 +129,19 @@ if __name__ == '__main__':
         valid_size = 14457
     else:
         os._exit(0)
-    model_name = 'dropout0.2'
+    model_name = 'd_0.2_w_1_2_10'
 
     batch_size = 72
     epochs = 100
 
+    class_weight = {0: 1.,1: 5.,2: 10.}
+
     train_loader = DataLoader(file_glob_pattern = 'feature/' + season + 'train.*.npy', batch_size = batch_size)
-    valid_loader = DataLoader(file_glob_pattern = 'feature/' + season + 'valid.*.npy', batch_size = batch_size)
+    valid_loader = DataLoader(file_glob_pattern = 'feature/' + season + 'valid.class_weight.*.npy', batch_size = batch_size)
     model_ckpt = ModelCheckpoint('model/'+ model_name + '.' + season + 'model.keras.h5', verbose = 1, save_best_only = True)
     tensorboard = TensorBoard(log_dir='./logs/' + season + model_name, histogram_freq=0, write_graph=True, write_images=False)
 
     model = build_model()
     model.compile(loss = 'categorical_crossentropy', optimizer = Adam(lr = 1e-4), metrics = ['accuracy'])
-    history = model.fit_generator(train_loader, steps_per_epoch = train_size // batch_size, epochs = epochs, validation_data = valid_loader, validation_steps = valid_size // batch_size
+    history = model.fit_generator(train_loader, class_weight=class_weight, steps_per_epoch = train_size // batch_size, epochs = epochs, validation_data = valid_loader, validation_steps = valid_size // batch_size
             , callbacks = [model_ckpt, tensorboard])
